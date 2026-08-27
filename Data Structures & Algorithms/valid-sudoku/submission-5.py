f7307b385272b39c1a0ef0958b1dee795dd6b046
Rama Ramana Sharma Parnandi <95123123+ramz-021002@users.noreply.github.com>
2026-08-26 21:17:33 -0400

    Add: valid-sudoku - submission-5

---
+++ Data Structures & Algorithms/valid-sudoku/submission-5.py
@@ -0,0 +1,44 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        condition1 = True
+        condition2 = True
+        condition3 = True
+        # Row checking and Column checks
+        for i in range(9):
+            row = board[i]
+            column = [row[i] for row in board]
+            count_row = Counter(row)
+            count_column = Counter(column)
+            # sorted_items_row = sorted(count_row.items(), key=lambda x: x[1], reverse=True)
+            # sorted_items_column = sorted(count_column.items(), key=lambda x: x[1], reverse=True)
+            
+            for num, freq in count_row.items():
+                if num != '.' and freq > 1:
+                    return False
+
+            for num, freq in count_column.items():
+                if num != '.' and freq > 1:
+                    return False
+        
+        # individual matrix check
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                matrix = []
+
+                for row in range(i,i+3):
+                    for column in range(j,j+3):
+                        matrix.append(board[row][column])
+                
+                count_matrix = Counter(matrix)
+                # sorted_items_matrix = sorted(count_matrix.items(), key=lambda x: x[1], reverse=True)
+
+                for num, freq in count_matrix.items():
+                    if num != '.' and freq > 1:
+                        return False
+
+
+        return True
+
+        
+            
+
